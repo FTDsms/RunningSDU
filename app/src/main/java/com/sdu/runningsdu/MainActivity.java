a5d7727faef0cdd7ca2ac5f217742adbe461623e
shaomingshan <FTDsms@outlook.com>
2018-06-08 10:03:50 +0800

    Update June 8th

--- app/src/main/java/com/sdu/runningsdu/MainActivity.java
+++ app/src/main/java/com/sdu/runningsdu/MainActivity.java
@@ -179,18 +179,19 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 // Handle navigation view item clicks here.
                 int id = item.getItemId();
-                if (id == R.id.nav_camera) {
-                    // Handle the camera action
-                } else if (id == R.id.nav_gallery) {
-
-                } else if (id == R.id.nav_slideshow) {
-
-                } else if (id == R.id.nav_manage) {
-
-                } else if (id == R.id.nav_share) {
-
-                } else if (id == R.id.nav_send) {
-
+                switch (id) {
+                    case R.id.nav_wallet:
+                        break;
+                    case R.id.nav_collection:
+                        break;
+                    case R.id.nav_album:
+                        break;
+                    case R.id.nav_card_bag:
+                        break;
+                    case R.id.nav_expression:
+                        break;
+                    case R.id.nav_setting:
+                        break;
                 }
                 DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
                 drawer.closeDrawer(GravityCompat.START);

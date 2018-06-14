8e8e14bd8979608aa8f469be342e4f691bc73830
shaomingshan <FTDsms@outlook.com>
2018-06-14 10:31:01 +0800

    Update on June 14th

--- app/src/main/java/com/sdu/runningsdu/Utils/MD5.java
+++ app/src/main/java/com/sdu/runningsdu/Utils/MD5.java
@@ -3,6 +3,9 @@ package com.sdu.runningsdu.Utils;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -12,6 +15,11 @@ import java.security.NoSuchAlgorithmException;
 
 public class MD5 {
 
+    /**
+     * 计算字符串 MD5 值
+     * @param string
+     * @return
+     */
     @NonNull
     public static String md5(String string) {
         if (TextUtils.isEmpty(string)) {
@@ -36,6 +44,12 @@ public class MD5 {
         return "";
     }
 
+    /**
+     * MD5 加盐算法
+     * @param string
+     * @param slat
+     * @return
+     */
     @NonNull
     public static String md5(String string, String slat) {
         if (TextUtils.isEmpty(string)) {
@@ -58,6 +72,67 @@ public class MD5 {
             e.printStackTrace();
         }
         return "";
+    }
+
+    /**
+     * 计算文件的 MD5 值
+     * @param file
+     * @return
+     */
+    @NonNull
+    public static String md5(File file) {
+        if (file == null || !file.isFile() || !file.exists()) {
+            return "";
+        }
+        FileInputStream in = null;
+        String result = "";
+        byte buffer[] = new byte[8192];
+        int len;
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            in = new FileInputStream(file);
+            while ((len = in.read(buffer)) != -1) {
+                md5.update(buffer, 0, len);
+            }
+            byte[] bytes = md5.digest();
+
+            for (byte b : bytes) {
+                String temp = Integer.toHexString(b & 0xff);
+                if (temp.length() == 1) {
+                    temp = "0" + temp;
+                }
+                result += temp;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            if(null!=in){
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 对字符串多次 MD5 加密
+     * @param string
+     * @param times
+     * @return
+     */
+    @NonNull
+    public static String md5(String string, int times) {
+        if (TextUtils.isEmpty(string)) {
+            return "";
+        }
+        String md5 = md5(string);
+        for (int i = 0; i < times - 1; i++) {
+            md5 = md5(md5);
+        }
+        return md5(md5);
     }
 
 }
